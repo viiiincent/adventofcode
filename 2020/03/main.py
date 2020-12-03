1241c52d471e00ec2c7ee7e5f1c3a60bfa8a0091
Vincent Marger <vincent.marger@gmail.com>
2020-12-03 12:11:07 -0500

    2020 03

---
+++ 2020/03/main.py
@@ -0,0 +1,41 @@
+
+def read_input(file_path):
+    lines = []
+    with open(file_path) as f:
+        for line in f:
+            lines.append([c for c in line.strip()])
+    return lines
+
+# for line in f:
+#     for c in line:
+
+# c for line in f for c in line
+
+def main(data, slopes):
+    map_width = len(data[0])
+    map_height = len(data)
+
+    print(map_width, map_height)
+
+    res = 1
+    for slope in slopes:
+        row, col = 0, 0
+        tree_count = 0
+        while row < (map_height-1):
+            row += slope[1]
+            col = (col + slope[0]) % map_width
+            # print(data[row])
+            # print("row: {}, col: {}".format(row, col))
+            if data[row][col] == '#':
+                tree_count += 1
+        print("{} tree_count: {}".format(slope, tree_count))
+        res *= tree_count
+    print("res: {}".format(res))
+
+
+
+
+if __name__ == "__main__":
+    data = read_input("./input.txt")
+    # main(data, [(3,1)])
+    main(data, [(1,1), (3,1), (5,1), (7,1), (1,2)]) 
